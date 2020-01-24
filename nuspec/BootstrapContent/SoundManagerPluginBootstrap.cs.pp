095a1fb5fb4b415bf70e40d4704c6495b17e0482
nicolaschapelier <44229494+nicolaschapelier@users.noreply.github.com>
2020-01-24 13:32:06 +0100

    Migrate to .NET Standard 2.0 and MvvmCross 6.4.2 (#5)
    * Migrate to .NET Standard 2.0 and MvvmCross 6.4.2

--- nuspec/BootstrapContent/SoundManagerPluginBootstrap.cs.pp
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
- namespace $rootnamespace$.Bootstrap
- {
-    public class SoundManagerPluginBootstrap
-        : MvxPluginBootstrapAction<MvvX.Plugins.SoundManager.PluginLoader>
-    {
-    }
- }
